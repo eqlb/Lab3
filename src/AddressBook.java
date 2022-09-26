731ded1e3a8aaf04327aff18a603a0012d54a669
kylet <kyletaticek@cmail.carleton.ca>
2022-09-26 15:55:46 -0400

    Edited AddressBook class

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,21 +1,28 @@
 import java.util.ArrayList;
 public class AddressBook {
-    private ArrayList<BuddyInfo> aBuddy;
+    private ArrayList<BuddyInfo> myBuddies;
 
     public AddressBook(){
-        this.aBuddy = new ArrayList<BuddyInfo>();
+
+        myBuddies = new ArrayList<>();
     }
-    public void addBuddy(BuddyInfo buddy){
-        this.aBuddy.add(buddy);
+    public void addBuddy(BuddyInfo aBuddy){
+        if(aBuddy !=null){
+            myBuddies.add(aBuddy);
+        }
+
     }
-    public void removeBuddy(BuddyInfo buddy){
-        this.aBuddy.remove(buddy);
+    public BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < myBuddies.size()){
+            return myBuddies.remove(index);
+        }
+        return null;
     }
 
     public static void main(String[] args){
-        BuddyInfo buddy = new BuddyInfo("Tom","Carleton",613);
+        BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
     }
 }
---
+++ src/BuddyInfo.java
@@ -0,0 +1,31 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public static void main(String[] args) {
+        // BuddyInfo y = new BuddyInfo();
+        // String buddy = "Jay";
+        // y.setName(buddy);
+        // System.out.println("Hello " + y.getName());
+    }
+}
